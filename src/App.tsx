1376cb91092308d6b4204413b3a8c69f15f43362
AleksandrFROLik <nextfrolik@gmail.com>
2021-11-22 22:19:26 +0300

    Portfolio path 2

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import {MainHeader} from "./components/header/MainHeader";
 import {Main} from "./components/main/Main";
+import {Skills} from "./components/skills/Skills";
 
 function App() {
     return (
         <div className="App">
             <MainHeader/>
             <Main/>
+            <Skills/>
         </div>
     );
 }
